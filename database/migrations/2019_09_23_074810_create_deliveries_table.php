87f3cf1843b0acca6ba8214192a333f6bc6c60d1
Unkn0wn <unkn0wn9@outlook.com>
2019-10-07 09:33:51 +0800

    修复调度中返回时间的格式

--- database/migrations/2019_09_23_074810_create_deliveries_table.php
+++ database/migrations/2019_09_23_074810_create_deliveries_table.php
@@ -18,7 +18,7 @@ class CreateDeliveriesTable extends Migration
             $table->unsignedInteger('car_id')->comment('车辆id');
             $table->unsignedInteger('guest_id')->comment('来宾信息id');
             $table->timestamp('start_time')->comment('出发时间')->default("2019-09-23 00:00:00");
-            $table->time('back_time')->comment('返回时间')->default("2019-09-23 00:00:00");
+            $table->timestamp('back_time')->comment('返回时间')->default("2019-09-23 00:00:00");
             $table->timestamps();
         });
     }
